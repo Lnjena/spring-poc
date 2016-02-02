fc0c07489db8cac22301d2394d6b35c8e7f58f5d
Lnjena.active@gmail.com <Lnjena.active@gmail.com>
2016-02-02 14:50:30 +0000

    spring boot security project init

---
+++ spring-security/src/test/java/com/poc/SpringSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.poc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringSecurityApplication.class)
+public class SpringSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
